0c64d0fd3c6b1805d218982a38e3f39ee90de7ba
kosakaproject <155091380+kosakaproject@users.noreply.github.com>
2024-01-02 12:40:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const progress_percentage = document.querySelector('.progress span');
 
 let per = 0;
 function progressLoad() {
-    if(per >= 30){
-        progress.style.width = '30%';
-        progress_percentage.innerHTML = "30%"
+    if(per >= 40){
+        progress.style.width = '40%';
+        progress_percentage.innerHTML = "40%"
     } else {
         progress.style.width = `${per}%`;
         progress_percentage.innerHTML = `${per}%`;
